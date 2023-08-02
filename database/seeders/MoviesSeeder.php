80ca9d99efcd58c9939952d1267ec0fb8b7934ec
Anieb22 <anielloverdino@gmail.com>
2023-08-02 16:46:03 +0200

    creata migration seeder e popolato il db

---
+++ database/seeders/MoviesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Movie;
+
+class MoviesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 6; $i++){
+        $newMovie = new Movie();
+        $newMovie->title = $faker->word();
+        $newMovie->image = $faker->imageUrl(640, 480, 'movies', true);;
+        $newMovie->description = $faker->paragraph();
+        $newMovie->director = $faker->word();
+        $newMovie->votes = $faker->numberBetween(1, 5);;
+        $newMovie->exit_date = $faker->dateTime();
+
+        $newMovie->save();
+    }
+    }
+}
